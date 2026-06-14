d7e1dd2457ecf73ffc57fb3cb36cf35b766db169
wyxdlz54188 <liyuqili_1379zi@qq.com>
2026-06-14 19:25:46 +0800

    update

--- Newterm/TermViewController.h
+++ Newterm/TermViewController.h
@@ -3,20 +3,19 @@
 @class TermView;
 @class SessionManager;
 @interface TermViewController : UIViewController
-
-@property (strong, nonatomic) TermView *termView;
-@property (strong, nonatomic) SessionManager *sessionManager;
-@property (strong, nonatomic) UIToolbar *toolbar;
-@property (strong, nonatomic) UIBarButtonItem *newTabButton;
-@property (strong, nonatomic) UIBarButtonItem *settingsButton;
-@property (strong, nonatomic) UIBarButtonItem *copyButton;
-@property (assign, nonatomic) BOOL isConnected;
-
-@end
-
-- (void)newTerminalSession;
-- (void)showSettings;
-- (void)copyTerminalText;
-- (void)pasteToTerminal;
-
-@end
+ 
+ @property (strong, nonatomic) TermView *termView;
+ @property (strong, nonatomic) SessionManager *sessionManager;
+ @property (strong, nonatomic) UIToolbar *toolbar;
+ @property (strong, nonatomic) UIBarButtonItem *newTabButton;
+ @property (strong, nonatomic) UIBarButtonItem *settingsButton;
+ @property (strong, nonatomic) UIBarButtonItem *copyButton;
+ @property (assign, nonatomic) BOOL isConnected;
+ 
+ // 方法声明
+ - (void)newTerminalSession;
+ - (void)showSettings;
+ - (void)copyTerminalText;
+ - (void)pasteToTerminal;
+ 
+ @end
