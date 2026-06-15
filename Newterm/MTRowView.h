7525f02e3705e5ccb3b491a12e6dd24146e997bb
wyxdlz54188 <liyuqili_1379zi@qq.com>
2026-06-15 23:01:27 +0800

    update

---
+++ Newterm/MTRowView.h
@@ -0,0 +1,13 @@
+#import <CoreText/CoreText.h>
+
+#define kMTBackgroundColorAttributeName CFSTR("MTBackgroundColor")
+#define kMTStrikethroughColorAttributeName CFSTR("MTStrikethroughColor")
+
+@interface MTRowView : UIView {
+  CGColorRef bgColor;
+  CTLineRef ctLine;
+  CGFloat lineAscent;
+  CFMutableDictionaryRef bgMap,stMap;
+}
+-(void)renderString:(CFAttributedStringRef)string withBGColor:(CGColorRef)_bgColor;
+@end
