3b38781edc3de6761d1a6d6ce9e104bcd7385730
wyxdlz54188 <liyuqili_1379zi@qq.com>
2026-06-19 16:48:07 +0800

    update

--- Newterm/MTAppDelegate.m
+++ Newterm/MTAppDelegate.m
@@ -2,6 +2,56 @@
 #import "MTController.h"
 #import "MTSettingsController.h"
 
+static UIImage* createTerminalIcon() {
+  UIGraphicsBeginImageContextWithOptions(CGSizeMake(30,30),NO,0);
+  CGContextRef ctx=UIGraphicsGetCurrentContext();
+
+  // 深色背景
+  [[UIColor colorWithWhite:0.15 alpha:1] setFill];
+  [[UIBezierPath bezierPathWithRoundedRect:CGRectMake(1,1,28,28) cornerRadius:6] fill];
+
+  // 绿色 >_ 文字
+  [[UIColor colorWithRed:0.3 green:0.85 blue:0.3 alpha:1] setFill];
+  [@">_" drawAtPoint:CGPointMake(5,5) withFont:[UIFont fontWithName:@"Courier-Bold" size:18]];
+
+  UIImage* img=UIGraphicsGetImageFromCurrentImageContext();
+  UIGraphicsEndImageContext();
+  return img;
+}
+
+static UIImage* createSettingsIcon() {
+  UIGraphicsBeginImageContextWithOptions(CGSizeMake(30,30),NO,0);
+  CGContextRef ctx=UIGraphicsGetCurrentContext();
+
+  CGFloat cx=15,cy=15,outerR=13,innerR=9;
+
+  // 齿轮齿
+  [[UIColor colorWithWhite:0.7 alpha:1] setFill];
+  for(int i=0;i<8;i++){
+    CGFloat a=i*M_PI/4-M_PI/2;
+    CGFloat w=3,h=5;
+    CGContextSaveGState(ctx);
+    CGContextTranslateCTM(ctx,cx+cosf(a)*innerR,cy+sinf(a)*innerR);
+    CGContextRotateCTM(ctx,a);
+    CGContextFillRect(ctx,CGRectMake(-w/2,-h/2,w,h));
+    CGContextRestoreGState(ctx);
+  }
+
+  // 外圈
+  [[UIColor colorWithWhite:0.5 alpha:1] setFill];
+  CGContextFillEllipseInRect(ctx,CGRectMake(cx-outerR,cy-outerR,outerR*2,outerR*2));
+  // 内圈
+  [[UIColor colorWithWhite:0.35 alpha:1] setFill];
+  CGContextFillEllipseInRect(ctx,CGRectMake(cx-innerR,cy-innerR,innerR*2,innerR*2));
+  // 中心圆
+  [[UIColor colorWithWhite:0.5 alpha:1] setFill];
+  CGContextFillEllipseInRect(ctx,CGRectMake(cx-4,cy-4,8,8));
+
+  UIImage* img=UIGraphicsGetImageFromCurrentImageContext();
+  UIGraphicsEndImageContext();
+  return img;
+}
+
 @implementation MTAppDelegate
 
 -(void)applicationDidFinishLaunching:(UIApplication*)application {
@@ -9,13 +59,25 @@
 
   controller=[[MTController alloc] init];
   controller.title=@"Term";
-  controller.tabBarItem=[[[UITabBarItem alloc] 
-    initWithTabBarSystemItem:UITabBarSystemItemDownloads tag:0] autorelease];
+  
+  UIImage* termIcon=createTerminalIcon();
+  UITabBarItem* termItem=[[UITabBarItem alloc] initWithTitle:@"Term" image:nil tag:0];
+  if([termItem respondsToSelector:@selector(setFinishedSelectedImage:withFinishedUnselectedImage:)]){
+    [termItem setFinishedSelectedImage:termIcon withFinishedUnselectedImage:termIcon];
+  }
+  controller.tabBarItem=termItem;
+  [termItem release];
 
   MTSettingsController* settingsController=[[MTSettingsController alloc] init];
   settingsController.title=@"设置";
-  settingsController.tabBarItem=[[[UITabBarItem alloc] 
-    initWithTabBarSystemItem:UITabBarSystemItemMore tag:1] autorelease];
+  
+  UIImage* settingsIcon=createSettingsIcon();
+  UITabBarItem* settingsItem=[[UITabBarItem alloc] initWithTitle:@"设置" image:nil tag:1];
+  if([settingsItem respondsToSelector:@selector(setFinishedSelectedImage:withFinishedUnselectedImage:)]){
+    [settingsItem setFinishedSelectedImage:settingsIcon withFinishedUnselectedImage:settingsIcon];
+  }
+  settingsController.tabBarItem=settingsItem;
+  [settingsItem release];
 
   tabBarController=[[UITabBarController alloc] init];
   tabBarController.viewControllers=[NSArray arrayWithObjects:controller,settingsController,nil];
