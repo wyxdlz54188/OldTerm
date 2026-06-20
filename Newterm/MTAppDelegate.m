1d5f4df6a64777efb371ba4955ea4f28e236cc0e
wyxdlz54188 <monkeycode-ai@chaitin.com>
2026-06-20 12:07:54 +0000

    feat: use PNG image for settings tab bar icon Co-authored-by: monkeycode-ai <monkeycode-ai@chaitin.com>

--- Newterm/MTAppDelegate.m
+++ Newterm/MTAppDelegate.m
@@ -18,53 +18,6 @@ static UIImage* createTerminalIcon() {
   return img;
 }
 
-static UIImage* createSettingsIcon() {
-  UIGraphicsBeginImageContextWithOptions(CGSizeMake(30,30),NO,0);
-
-  CGFloat cx=15,cy=15,outerR=13,innerR=9;
-
-  UIBezierPath* gear=[UIBezierPath bezierPath];
-  NSInteger teeth=8;
-
-  for(NSInteger i=0;i<teeth;i++){
-    CGFloat a=i*M_PI*2/teeth-M_PI/2;
-    CGFloat sa=a-0.15,ea=a+0.15;
-    CGFloat innerX1=cx+cosf(sa)*innerR,innerY1=cy+sinf(sa)*innerR;
-    CGFloat innerX2=cx+cosf(ea)*innerR,innerY2=cy+sinf(ea)*innerR;
-    CGFloat outerX1=cx+cosf(sa)*outerR,outerY1=cy+sinf(sa)*outerR;
-    CGFloat outerX2=cx+cosf(ea)*outerR,outerY2=cy+sinf(ea)*outerR;
-
-    if(i==0){
-      [gear moveToPoint:CGPointMake(innerX1,innerY1)];
-      [gear addLineToPoint:CGPointMake(outerX1,outerY1)];
-    }
-    [gear addLineToPoint:CGPointMake(outerX2,outerY2)];
-    [gear addLineToPoint:CGPointMake(innerX2,innerY2)];
-
-    CGFloat na=i*M_PI*2/teeth-M_PI/2+M_PI/teeth;
-    CGFloat niX=cx+cosf(na)*innerR,niY=cy+sinf(na)*innerR;
-    [gear addQuadCurveToPoint:CGPointMake(niX,niY)
-                 controlPoint:CGPointMake(cx+cosf(na-M_PI/teeth*0.5)*(innerR+1),
-                                          cy+sinf(na-M_PI/teeth*0.5)*(innerR+1))];
-  }
-  [gear closePath];
-
-  [[UIColor colorWithWhite:0.55 alpha:1] setFill];
-  [gear fill];
-
-  UIBezierPath* center=[UIBezierPath bezierPathWithOvalInRect:CGRectMake(cx-4,cy-4,8,8)];
-  [[UIColor colorWithWhite:0.35 alpha:1] setFill];
-  [center fill];
-
-  UIBezierPath* dot=[UIBezierPath bezierPathWithOvalInRect:CGRectMake(cx-2,cy-2,4,4)];
-  [[UIColor colorWithWhite:0.6 alpha:1] setFill];
-  [dot fill];
-
-  UIImage* img=UIGraphicsGetImageFromCurrentImageContext();
-  UIGraphicsEndImageContext();
-  return img;
-}
-
 @implementation MTAppDelegate
 
 -(void)applicationDidFinishLaunching:(UIApplication*)application {
@@ -89,11 +42,8 @@ static UIImage* createSettingsIcon() {
   settingsNav.navigationBar.barStyle=UIBarStyleDefault;
   [settingsController release];
   
-  UIImage* settingsIcon=createSettingsIcon();
-  UITabBarItem* settingsItem=[[UITabBarItem alloc] initWithTitle:@"设置" image:nil tag:1];
-  if([settingsItem respondsToSelector:@selector(setFinishedSelectedImage:withFinishedUnselectedImage:)]){
-    [settingsItem setFinishedSelectedImage:settingsIcon withFinishedUnselectedImage:settingsIcon];
-  }
+  UIImage* settingsIcon=[UIImage imageNamed:@"tab_settings"];
+  UITabBarItem* settingsItem=[[UITabBarItem alloc] initWithTitle:@"设置" image:settingsIcon tag:1];
   settingsNav.tabBarItem=settingsItem;
   [settingsItem release];
 
