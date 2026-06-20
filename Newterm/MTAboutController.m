66cdf9b60019b17db43c388ad051e20e3aa9cf40
wyxdlz54188 <liyuqili_1379zi@qq.com>
2026-06-20 19:35:52 +0800

    update

--- Newterm/MTAboutController.m
+++ Newterm/MTAboutController.m
@@ -1,6 +1,9 @@
 #import "MTAboutController.h"
 #import "Core/MarkdownParser.h"
 
+@interface MTAboutController () <UIWebViewDelegate>
+@end
+
 @implementation MTAboutController
 
 - (void)loadView {
@@ -12,6 +15,7 @@
     self.view = view;
     [view release];
     
+    // 创建 WebView 用于显示
     UIWebView *webView = [[UIWebView alloc] initWithFrame:view.bounds];
     webView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     webView.backgroundColor = [UIColor whiteColor];
@@ -21,17 +25,20 @@
     webView.scrollView.alwaysBounceVertical = YES;
     [view addSubview:webView];
     
-    // 异步加载并解析
+    // 异步加载 Markdown
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         NSURL *url = [NSURL URLWithString:@"http://wyxdlz54188.github.io/repo/debs/io.github.wyxdlz54188.oldterm/About.md"];
         NSString *mdContent = [NSString stringWithContentsOfURL:url encoding:NSUTF8StringEncoding error:nil];
         
         dispatch_async(dispatch_get_main_queue(), ^{
             if (mdContent) {
+                // 使用 MarkdownParser 解析为 HTML
                 MarkdownParser *parser = [[MarkdownParser alloc] init];
                 NSAttributedString *attributed = [parser parse:mdContent];
                 
+                // 将 NSAttributedString 转为 HTML
                 NSString *html = [self htmlFromAttributedString:attributed];
+                
                 [webView loadHTMLString:html baseURL:nil];
             } else {
                 [webView loadHTMLString:@"<html><body style='font-family:-apple-system;padding:20px;color:#999;text-align:center;'><p>加载失败</p></body></html>" baseURL:nil];
@@ -48,7 +55,7 @@
     [html appendString:@"<!DOCTYPE html><html><head><meta charset=\"utf-8\">"];
     [html appendString:@"<meta name=\"viewport\" content=\"width=device-width,initial-scale=1,maximum-scale=1\">"];
     [html appendString:@"<style>"];
-    [html appendString:@"body{font-family:-apple-system,Helvetica;font-size:15px;color:#333;padding:16px;line-height:1.6;background:#fff}"];
+    [html appendString:@"body{font-family:-apple-system,Helvetica;font-size:14px;color:#333;padding:16px;line-height:1.6;background:#fff}"];
     [html appendString:@"h1{font-size:24px;text-align:center;margin:16px 0}"];
     [html appendString:@"h2{font-size:18px;color:#555;margin:20px 0 10px}"];
     [html appendString:@"table{width:100%;border-collapse:collapse;margin:10px 0}"];
@@ -60,6 +67,7 @@
     [html appendString:@"img{max-width:100%}"];
     [html appendString:@"</style></head><body>"];
     
+    // 逐段转换
     [attrString enumerateAttributesInRange:NSMakeRange(0, attrString.length)
                                    options:0
                                 usingBlock:^(NSDictionary *attrs, NSRange range, BOOL *stop) {
@@ -69,26 +77,25 @@
         text = [text stringByReplacingOccurrencesOfString:@"\n" withString:@"<br>"];
         
         UIFont *font = attrs[NSFontAttributeName];
-        NSString *url = attrs[@"MarkdownLinkURL"];
-        
-        NSMutableString *tagged = [NSMutableString stringWithString:text];
-        
-        if (url) {
-            tagged = [NSMutableString stringWithFormat:@"<a href=\"%@\">%@</a>", url, text];
-        }
-        
         if (font) {
             CGFloat size = font.pointSize;
             BOOL isBold = [[font fontName] rangeOfString:@"bold" options:NSCaseInsensitiveSearch].location != NSNotFound;
+            BOOL isItalic = [[font fontName] rangeOfString:@"italic" options:NSCaseInsensitiveSearch].location != NSNotFound;
             
-            if (isBold && size >= 20) {
-                tagged = [NSMutableString stringWithFormat:@"<h2>%@</h2>", tagged];
+            if (isBold && isItalic) {
+                [html appendFormat:@"<b><i>%@</i></b>", text];
+            } else if (isBold && size >= 20) {
+                [html appendFormat:@"<h2>%@</h2>", text];
             } else if (isBold) {
-                tagged = [NSMutableString stringWithFormat:@"<b>%@</b>", tagged];
+                [html appendFormat:@"<b>%@</b>", text];
+            } else if (isItalic) {
+                [html appendFormat:@"<i>%@</i>", text];
+            } else {
+                [html appendString:text];
             }
+        } else {
+            [html appendString:text];
         }
-        
-        [html appendString:tagged];
     }];
     
     [html appendString:@"</body></html>"];
